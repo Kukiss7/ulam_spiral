875f1c0131d98d60e5c8370a30df1f5024fe95dc
Łukasz Janicki <janicki_lukasz@o2.pl>
2018-08-20 09:56:27 +0200

    updated is_prime function

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,12 +1,14 @@
 
 
 def is_prime(number):
-	if number < 0:
+	""" Checks if given number is a prime number"""
+	if not isinstance(number, int):
+		raise TypeError("The number must be an int number")
+	elif number < 0:
 		return False
 	elif number in [0,1]:
 		return False
-	elif not isinstance(number, int):
-		raise TypeError("The number must be an int number")
+
 	for n in range(2, number):
 		if number % n == 0:
 			return False
