0f37c344b7188e75c17d3b9860d62ed367f660a9
Łukasz Janicki <janicki_lukasz@o2.pl>
2018-08-20 11:05:05 +0200

    updated is_prime function

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,6 +1,6 @@
+import math
 
-
-def is_prime(number):
+def is_prime_basic(number):
 	""" Checks if given number is a prime number"""
 	if not isinstance(number, int):
 		raise TypeError("The number must be an int number")
@@ -9,7 +9,8 @@ def is_prime(number):
 	elif number in [0,1]:
 		return False
 
-	for n in range(2, number):
+	max_divider = int(math.sqrt(number))
+	for n in range(2, max_divider):
 		if number % n == 0:
 			return False
 		else:
